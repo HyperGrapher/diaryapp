f45d65692bdecb7482868444b715c3ede2c7e26e
Burak Mestan <burakmestan@msn.com>
2015-07-29 20:38:23 +0300

    initial commitim

---
+++ Diary.py
@@ -0,0 +1,52 @@
+#!usr/bin/python
+
+#29-07-2015 18:57:59 
+
+from peewee import *
+import datetime
+
+db = SqliteDatabase("diary.db")
+    
+class Entry(Model):
+    content = TextField()
+    timestamp = DateTimeField(default=datetime.datetime.now)
+    
+    
+    
+    
+    class Meta:
+        database = db
+ 
+def initialize():
+    """create the db and table if they don't exist"""
+    db.connect()
+    db.create([Entry], safe=True)
+           
+        
+def menu_loop():
+    """shows menu"""
+    
+    
+def add_entry():
+    """adds entry"""
+    
+def view_entries():
+    "view entry"
+    
+def delete_entry():
+    "deletes entry"
+    
+    
+
+
+if __name__ == '__main__':
+    initialize()
+    print("burak")
+
+    
+
+
+    
+
+
+
